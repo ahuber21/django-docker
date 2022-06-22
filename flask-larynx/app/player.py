2afe59d4703b6ebf69267cbc6f9c9d363765e4d4
Andreas Huber <andi@saufhaengerle-test>
2022-06-22 08:06:07 +0200

    Playback MP3 in Alpine Docker container

--- flask-larynx/app/player.py
+++ flask-larynx/app/player.py
@@ -3,6 +3,7 @@ from time import sleep
 
 import pygame
 
+pygame.mixer.pre_init(buffer=1512)
 pygame.mixer.init()
 
 
@@ -14,3 +15,13 @@ def play(wav: bytes) -> None:
         pygame.mixer.music.play()
         while pygame.mixer.music.get_busy() == True:
             sleep(0.01)
+
+
+if __name__ == "__main__":
+    # downloaded from https://www.kozco.com/tech/piano2-Audacity1.2.5.mp3
+    pygame.mixer.music.load("piano2-Audacity1.2.5.mp3")
+    print("Audio loaded")
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        sleep(0.1)
+    print("Successfully played the file")
